3d5d2c43d9c0e9539f1ba84cb34fe24a7b0fb90d
Sandeep Sokle <sandeepsokle12@gmail.com>
2023-06-06 10:28:13 +0530

    sandeep || add upload router fron asure done router : http://localhost:5000/api/v1/upload

---
+++ Routes/uploadRoutes.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const azure = require("azure-storage");
+const multer = require("multer");
+const { Readable } = require("stream");
+const connectionString = `DefaultEndpointsProtocol=https;AccountName=hiringapp;AccountKey=M6IwLwYDW2orKumKKeAfuQ+wmMoP3iLP8MTNzpYC08P8jrA7iaVis9uh6Te1vnFFE+nTfESrh/Ba+AStwOU9BQ==;EndpointSuffix=core.windows.net`;
+const blobService = azure.createBlobService(connectionString);
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+router.route("/").post(upload.single("file"), (req, res) => {
+  console.log(req);
+  const containerName = "hiringappdata";
+  const fileName = new Date().valueOf() + "_" + req.file.originalname;
+  const fileData = req.file.buffer;
+  const stream = Readable.from(fileData);
+  // Create the container if it doesn't exist
+  blobService.createContainerIfNotExists(
+    containerName,
+    { publicAccessLevel: "blob" },
+    (error) => {
+      if (error) {
+        console.error("Error creating container:", error);
+        res.status(500).send("Error creating container");
+      } else {
+        // Upload the file to the container
+        blobService.createBlockBlobFromStream(
+          containerName,
+          fileName,
+          stream,
+          fileData.length,
+          (err, result, response) => {
+            if (err) {
+              console.error("Error uploading File:", err);
+              res.status(500).send("Error uploading File");
+            } else {
+              const fileUrl = `https://hiringapp.blob.core.windows.net/${result.container}/${result.name}`;
+              console.log("File URL:", fileUrl);
+              console.log("File uploaded successfully.");
+              res
+                .status(200)
+                .send({ msg: "File uploaded successfully", fileUrl });
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 const express = require("express");
 // const bodyParser = require("body-parser");
-const morgan = require("morgan")
-const cors = require("cors")
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const participantRouter = require("./Routes/ParticipantRoutes");
 const subjectRouter = require("./Routes/SubjectRoutes");
 const superAdminRoutes = require("./Routes/SuperAdminRoutes");
-const sampleRouter = require('./Routes/SampleRoutes')
-const userRoutes = require("./Routes/UserRoutes")
+const sampleRouter = require("./Routes/SampleRoutes");
+const userRoutes = require("./Routes/UserRoutes");
+const uploadRoutes = require("./Routes/uploadRoutes");
 const app = express();
 dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(morgan('common'))
+app.use(morgan("common"));
 app.use(cors());
 
-mongoose.connect(process.env.Mongo_DB, {
+mongoose
+  .connect(process.env.Mongo_DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
+  })
 
-.then(() => {
-console.log('DataBase Connected Successfully')
-})
+  .then(() => {
+    console.log("DataBase Connected Successfully");
+  })
 
-.catch((error) => {
-        console.log(error);
-    })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(process.env.PORT, () => {
-    console.log("the Server is Running on this Port", process.env.PORT);
-})
+  console.log("the Server is Running on this Port", process.env.PORT);
+});
 
 // All Routes
-app.use("/api/v1/participant", participantRouter)
-app.use("/api/v1/sample", sampleRouter)
-app.use("/api/v1/subject", subjectRouter)
-app.use("/api/v1/admin", superAdminRoutes)
-app.use("/api/v1/user", userRoutes)
-
-
-
-
+app.use("/api/v1/participant", participantRouter);
+app.use("/api/v1/sample", sampleRouter);
+app.use("/api/v1/subject", subjectRouter);
+app.use("/api/v1/admin", superAdminRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/upload", uploadRoutes);
